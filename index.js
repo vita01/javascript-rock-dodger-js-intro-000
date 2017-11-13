a724361732fed37ecc172806fc00df47543b3ffb
Victoria <vitalina27@gmail.com>
2017-11-13 12:03:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -137,6 +137,16 @@ game.appendChild(rock);
  * Finally, alert "YOU LOSE!" to the player.
  */
 function endGame() {
+  clearInterval(gameInterval)
+
+ ROCKS.forEach(function(rock) {rock.remove() })
+
+ document.removeEventListener('keydown', moveDodger)
+
+ START.innerHTML = 'Play again ?';
+ START.style.display = 'inline';
+
+ return alert('YOU LOSE!');
 }
 
 function moveDodger(e) {
