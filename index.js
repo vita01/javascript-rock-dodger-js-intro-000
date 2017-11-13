eaf560849eff6c8d0901d2f9141896bda2cf48fa
Victoria <vitalina27@gmail.com>
2017-11-13 12:18:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ function endGame() {
 
   ROCKS.forEach(function(rock) {rock.remove() })
 
-  document.removeEventListener('keydown', moveDodger);
+  document.removeEventListener('keydown', moveDodger());
 
   START.innerHTML = 'Play again?';
   START.style.display = 'inline';
